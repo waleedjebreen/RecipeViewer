a599d62b5331543c98275a091c394ab3be94f0b4
Waleed Saad Jebreen <waleedjebreen@gmail.com>
2025-02-05 11:52:32 +0300

    added RecipeResponse model

---
+++ RecipeViewer/Models/RecipeResponse.swift
@@ -0,0 +1,32 @@
+//
+//  Untitled.swift
+//  RecipeViewer
+//
+//  Created by Waleed Jebrin on 05/02/2025.
+//
+
+struct RecipeResponse: Codable {
+    let recipes : [Recipe]?
+    let total : Int?
+    let skip : Int?
+    let limit : Int?
+}
+
+struct Recipe : Codable {
+    let id : Int?
+    let name : String?
+    let ingredients : [String]?
+    let instructions : [String]?
+    let prepTimeMinutes : Int?
+    let cookTimeMinutes : Int?
+    let servings : Int?
+    let difficulty : String?
+    let cuisine : String?
+    let caloriesPerServing : Int?
+    let tags : [String]?
+    let userId : Int?
+    let image : String?
+    let rating : Double?
+    let reviewCount : Int?
+    let mealType : [String]?
+}

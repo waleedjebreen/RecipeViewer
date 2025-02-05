409afc552ebf3c58ed955911f7e69815ff55ca2c
Waleed Saad Jebreen <waleedjebreen@gmail.com>
2025-02-05 11:45:28 +0300

    Initial Commit

---
+++ RecipeViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeViewer
+//
+//  Created by Waleed Jebrin on 05/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeViewer/RecipeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeViewerApp.swift
+//  RecipeViewer
+//
+//  Created by Waleed Jebrin on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
